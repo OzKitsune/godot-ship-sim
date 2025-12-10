633665f899644a5ce91bd004a52abb6b57c5deed
OzKitsune <54425262+OzKitsune@users.noreply.github.com>
2025-12-10 14:44:52 +1000

    Update demo

--- demo/Demo.cs
+++ demo/Demo.cs
@@ -3,35 +3,35 @@ using System;
 
 public partial class Demo : Node2D
 {
-	[Export] private Ship Ship;
-    [Export] private Map Map;
+	[Export] private Ship _ship;
+    [Export] private Map _map;
 
     public override void _Ready()
 	{
-        Map.Grid.GridClicked += OnGridClicked;
-        Ship.AutopilotTargetReached += OnShipAutopilotTargetReached;
-        Ship.AutopilotDisengaged += OnShipAutopilotDisengaged;
+        _map.Grid.GridClicked += OnGridClicked;
+        _ship.AutopilotTargetReached += OnShipAutopilotTargetReached;
+        _ship.AutopilotDisengaged += OnShipAutopilotDisengaged;
     }
 
     private void OnGridClicked(Vector2 worldPosition)
     {
         GD.Print($"Grid clicked: {worldPosition}");
 
-        Ship.EnableAutopilot(worldPosition);
+        _ship.EnableAutopilot(worldPosition);
     }
 
     private void OnShipAutopilotDisengaged(bool reachedTarget)
     {
-        Map.Grid.ClearMarker();
+        _map.Grid.ClearMarker();
     }
 
     private void OnShipAutopilotTargetReached()
     {
-        Map.Grid.ClearMarker();
+        _map.Grid.ClearMarker();
     }
 
     public override void _PhysicsProcess(double delta)
     {
-        Map.Grid.UpdateGrid(Ship.GlobalPosition);
+        _map.Grid.UpdateGrid(_ship.GlobalPosition);
     } 
 }
